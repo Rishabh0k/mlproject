47c15636926741c041c5828994fb7ba1d6de8588
Rishabh Kanaujiya <officialrishabh013@gmail.com>
2026-06-25 00:07:44 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -29,7 +29,7 @@ class DataIngestion:
             logging.info("Train test split inintiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.indestion_config.test.data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             return(
                 self.ingestion_config.train_data_path,
