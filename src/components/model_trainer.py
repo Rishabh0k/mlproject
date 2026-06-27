52271b8d847e1b74340b0c1e3607aa46fb23be00
Rishabh Kanaujiya <officialrishabh013@gmail.com>
2026-06-27 23:21:29 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -36,17 +36,71 @@ class ModelTrainer:
                 test_array[:,-1]
             )
             models = {
-                "Random Forest": RandomForestRegressor(),
-                "Decision tree": DecisionTreeRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "Linear Regression": LinearRegression(),
-                "K-Neighbors Regressor": KNeighborsRegressor(),
-                "XGBClassifier": XGBRegressor(),
-                "CatBoost Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor(),
+                    "Random Forest": RandomForestRegressor(),
+                    "Decision Tree": DecisionTreeRegressor(),
+                    "Gradient Boosting": GradientBoostingRegressor(),
+                    "Linear Regression": LinearRegression(),
+                    "K-Neighbors Regressor": KNeighborsRegressor(),
+                    "XGB Regressor": XGBRegressor(),
+                    "CatBoost Regressor": CatBoostRegressor(verbose=False),
+                    "AdaBoost Regressor": AdaBoostRegressor(),
+                }
+            params = {
+
+                "Decision Tree": {
+                    "criterion": ["squared_error",],
+                    "max_depth": [3, 5, 7, 10],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4]
+                },
+
+                "Random Forest": {
+                    "n_estimators": [50, 100, 200],
+                    "max_depth": [5, 10, 15],
+                    "min_samples_split": [2, 5],
+                    "min_samples_leaf": [1, 2],
+                    "max_features": ["sqrt"]
+                },
+
+                "Gradient Boosting": {
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "n_estimators": [100, 200],
+                    "max_depth": [3, 5],
+                    "subsample": [0.8, 1.0]
+                },
+
+                "Linear Regression": {
+                    "fit_intercept": [True, False]
+                },
+
+                "K-Neighbors Regressor": {
+                    "n_neighbors": [3, 5, 7, 9],
+                    "weights": ["uniform", "distance"],
+                    "p": [1, 2]
+                },
+
+                "XGB Regressor": {
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "n_estimators": [100, 200],
+                    "max_depth": [3, 4, 5],
+                    "subsample": [0.8, 1],
+                    "colsample_bytree": [0.8, 1]
+                },
+
+                "CatBoost Regressor": {
+                    "iterations": [100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "depth": [4, 6, 8]
+                },
+
+                "AdaBoost Regressor": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1, 1]
+                }
             }
 
-            model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
+                                             models=models,params=params)
 
             #to get best model score from dict
             best_model_name = max(model_report, key=model_report.get)
